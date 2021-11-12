cb94d72c7c8ec63ca21f5aef5bb8ee47fca0ef0f
AL MAHMUD SURUJ <mahmudsuruj14@gmail.com>
2021-11-13 06:00:00 +0800

    .env fixed

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-## [The Car Corner](https://.web.app/)
-  Welcome to the My Website.This is BMD car sell and repairing shop.Our main goal is to satisfy the customer.
+## The Car Corner [Click here to go my website](https://bmw-car-corners841.netlify.app/)
+  Welcome to the My Website.This is BMW car sell and repairing shop.Our main goal is to satisfy the customer.
 <br/>
 <br/>
 
@@ -9,7 +9,7 @@
 - Any Customer can book service, check status, and able to give their feedback.
 - Only Admin can add or remove services, add new admin, and also change order status.
 
-### Technology:
+### Technology Used in My Website:
 - React js
 - React Router
 - React Router Dom
--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 
 const app = firebase.initializeApp({
-    apiKey: "AIzaSyBidGaM_H6CKij-GLfPUCGBH1Wlk-28ej8",
-    authDomain: "my-car-corner.firebaseapp.com",
-    projectId: "my-car-corner",
-    storageBucket: "my-car-corner.appspot.com",
-    messagingSenderId: "781179696195",
-    appId: "1:781179696195:web:526c082e568ac908a490f6"
+    apiKey:process.env.REACT_APP_API_KEY,
+    authDomain:process.env.REACT_APP_AUTH_DOMAIN,
+    projectId:process.env.REACT_APP_PROJECT_ID,
+    storageBucket:process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId:process.env.REACT_APP_APP_ID,
 });
 
 export const auth = app.auth();
